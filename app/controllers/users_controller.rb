1111180f52873d76e7e0be1691d37cad08206215
Trieu Nguyen <trieunv@imt-soft.com>
2021-03-29 22:05:40 +0700

    add index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController<ApplicationController
 
+
+  def index
+    @user = User.paginate(page: params[:page], per_page: 5)
+  end
   def new
     @user = User.new
   end
@@ -22,7 +26,7 @@ class UsersController<ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Update successfully"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
@@ -30,7 +34,7 @@ class UsersController<ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private
